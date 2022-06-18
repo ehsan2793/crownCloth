ecc3756cd995b603b239d4c2359d8b5df9dc4c45
Ehsan Rahimi <ehsanrahimi@Ehsans-MacBook-Pro.local>
2022-06-17 18:33:00 -0700

    fix

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,13 +1,6 @@
-
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
-
-
-
-
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const config = {
     apiKey: "AIzaSyB00iaLu2PBEHnYe27GvAUMVf18JhkGUuo",
@@ -16,16 +9,12 @@ const config = {
     storageBucket: "crown-2d51b.appspot.com",
     messagingSenderId: "659292729098",
     appId: "1:659292729098:web:31e81d425cfb0b857554f3",
-    measurementId: "G-FNLF3LG5LS"
+    measurementId: "G-FNLF3LG5LS",
 };
-
-
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
-
     const userReft = firestore.doc(`users/${userAuth.uid}`);
-
     const snapShot = await userReft.get();
     if (!snapShot.exists) {
         const { displayName, email } = userAuth;
@@ -35,35 +24,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
                 displayName,
                 email,
                 createdAt,
-                ...additionalData
+                ...additionalData,
             });
+        } catch (error) {
+            console.log("error creating user", error.message);
         }
-        catch (error) {
-            console.log('error creating user', error.message);
-        }
-
-
-
     }
     return userReft;
-}
-
-
-
-
-
-
-
+};
 
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-
 export default firebase;
-
